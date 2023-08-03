cd889e832d77afdd09a2c103f2fc450ae6503e0c
Abdallah Massri <a.v.e@live.com>
2023-08-03 18:11:25 +0300

    updating the view of the login

--- src/main/java/com/example/hrpulse/HelloApplication.java
+++ src/main/java/com/example/hrpulse/HelloApplication.java
@@ -8,17 +8,26 @@ import javafx.stage.Stage;
 
 import java.io.IOException;
 
-
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("login-view.fxml"));
+
+
         Parent root = fxmlLoader.load();
-        Scene scene = new Scene(root, 800, 600);
-        stage.setTitle("Hello!");
+        Scene scene = new Scene(root, 600, 500);
+        stage.setTitle("Hr-Pulse");
+
         stage.setScene(scene);
+
+// Prevent the stage from being resizable
+        stage.setResizable(false);
+
+// Center the title in the stage
+        stage.centerOnScreen();
+
         stage.show();
+
     }
 
     public static void main(String[] args) {
