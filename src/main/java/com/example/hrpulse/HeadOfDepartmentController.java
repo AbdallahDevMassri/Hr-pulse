925045297458de5444ed7c4e967b7820e68d4e26
Sergey Morozov <globalvpsm@gmail.com>
2023-08-05 23:37:15 +0300

    manager options fixed, head log-in fixed, small issues fixed

--- src/main/java/com/example/hrpulse/HeadOfDepartmentController.java
+++ src/main/java/com/example/hrpulse/HeadOfDepartmentController.java
@@ -1,4 +1,14 @@
 package com.example.hrpulse;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
 public class HeadOfDepartmentController {
+    @FXML
+    private Label welcomeLabel;
+
+    public void initialize() {
+        welcomeLabel.setText("Welcome, Head of Department!");
+    }
 }
+
