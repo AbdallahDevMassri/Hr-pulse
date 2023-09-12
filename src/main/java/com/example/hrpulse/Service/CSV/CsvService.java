7121919907bc819ea2d14e4979ce0baa824b61c6
Sergey Morozov <globalvpsm@gmail.com>
2023-09-12 19:27:58 +0300

    Csv reader added

---
+++ src/main/java/com/example/hrpulse/Service/CSV/CsvService.java
@@ -0,0 +1,55 @@
+package com.example.hrpulse.Service.CSV;
+
+
+import com.opencsv.CSVReader;
+import com.opencsv.exceptions.CsvException;
+
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.List;
+
+public class CsvService {
+
+    /**
+     * Reads a CSV file and returns its contents as a List of String arrays.
+     *
+     * @param filePath the file path of the CSV file to read
+     * @return a List of String arrays representing the CSV data
+     * @throws IOException if an I/O error occurs while reading the file
+     */
+    public static List<String[]> readCsv(String filePath) throws IOException {
+
+        // Create a new CSVReader object using the given file path
+        CSVReader reader = new CSVReader(new FileReader(filePath));
+
+        List<String[]> csvData = null;
+        try {
+            // Read all the data from the CSV file into a List of String arrays
+            csvData = reader.readAll();
+        } finally {
+            // Close the CSVReader to release system resources
+            reader.close();
+        }
+
+        return csvData;
+    }
+
+    /*
+     * Prints the contents of a CSV file to the console.
+     *
+     * @param csvData a List of String arrays representing the CSV data to print
+     */
+    public static void printCsv(List<String[]> csvData) {
+        for (String[] row : csvData) {
+            for (String cell : row) {
+                // Print each cell followed by a tab character
+                System.out.print(cell + "\t");
+            }
+            // Print a new line character to separate rows
+            System.out.println();
+        }
+    }
+}
+
+
+
